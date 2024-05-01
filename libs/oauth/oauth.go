0be8820472f7961f9c27f26430684874e3f9631a
Chih-Han Liu <andyliu@gate.sinica.edu.tw>
2024-05-01 11:35:16 +0800

    <docs>add Oauth

---
+++ libs/oauth/oauth.go
@@ -0,0 +1,41 @@
+package Oauth
+
+import(
+	"os"
+   "strings"
+	"net/http"
+)
+
+type Oauth2 struct {
+   ClientID [string]			// ClientID is the application's ID.
+	ClientSecret [string]	// ClientSecret is the application's secret.
+	Endpoint [string]
+	RedirectUri [string]		// RedirectURL is the URL to redirect users going through the OAuth flow, after the resource owner's URLs.
+	Scopes [][string]			// Scope specifies optional requested permissions []string{"email", "profile"},
+}
+
+// http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+// code := r.URL.Query().Get("code")
+func googleHandler(w http.ResponseWriter, r *http.Request) {
+   // url := Oauth2.AuthCodeURL("state", oauth2.AccessTypeOffline)
+   // http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func AddRouter(router *mux.Router) {
+   // router.HandleFunc("GET /auth/login/google", googleHandler)
+}
+
+func NewOauth(clientID, clientSecret, redirectUri, scopes string)(*Oauth2, error) {
+	endpoint := os.Getenv("EndPoint")	
+	if endpoint == "" || clientID == "" || clientSecret == "" || redirectUri == "" || scopes == "" {
+		return nil, errors.New("Missing required parameters")
+	}
+   sps := strings.Split(scopes, ",")
+	return &Oauth2{
+		ClientID: clientID,
+		ClientSecret: clientSecret,
+		Endpoint: "https://api.twitter.com/oauth2/token",
+		RedirectUri: redirectUri,
+		Scopes: sps,
+	}, nil
+}
