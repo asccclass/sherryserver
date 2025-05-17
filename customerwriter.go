74aa296ed5b698d3ff1945d328c6d5f03d281ad8
andyliu <justgps@gmail.com>
2025-05-17 10:19:46 +0800

    <feature>增加可以自訂writer功能

---
+++ customerwriter.go
@@ -0,0 +1,45 @@
+/*
+   reference: https://anto.pt/articles/go-http-responsewriter?ref=dailydev
+*/
+package SherryServer
+
+import(
+   "net/http"
+)
+
+// it's actually a good idea to implement a Flusher version for our writer as well
+type HttpWriterFlusher struct {
+   *HttpWriter   // wrap our "normal" writer
+   http.Flusher  // keep a ref to the wrapped Flusher
+}
+
+// implement http.ResponseWriter
+func(w *HttpWriter) Header()(http.Header) {
+   return w.w.Header()
+}
+
+func(w *HttpWriter) Write(data []byte) (int, error) {
+   return w.w.Write(data)
+}
+
+func(w *HttpWriter) WriteHeader(statusCode int) {
+   w.w.WriteHeader(statusCode)
+}
+
+func(w *HttpWriterFlusher) Flush() {
+    w.Flusher.Flush()
+}
+
+// modify the constructor to either return HttpWriter or HttpWriterFlusher depending on the writer being wrapped
+func NewHttpWriter(w http.ResponseWriter)(http.ResponseWriter) {
+   httpWriter := &HttpWriter {
+      w: w,
+   }
+   if flusher, ok := w.(http.Flusher); ok {
+      return &HttpWriterFlusher{
+         HttpWriter: httpWriter,
+         Flusher:    flusher,
+      }
+   }
+   return httpWriter
+}
